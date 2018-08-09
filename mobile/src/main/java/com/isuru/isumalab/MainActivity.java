c1721b31df5e3d02d1313fcfdb11b03cc3b9ac92
IsuruNuwanthilaka <isurunuwanthilaka@gmail.com>
2018-08-09 17:03:28 +0530

    Activity > Added Youtube View Layout > Added Youtube layout

--- mobile/src/main/java/com/isuru/isumalab/MainActivity.java
+++ mobile/src/main/java/com/isuru/isumalab/MainActivity.java
@@ -8,13 +8,53 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mTopToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            Toast.makeText(MainActivity.this, "Action : Search clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        if (id == R.id.action_user) {
+            Toast.makeText(MainActivity.this, "Action : User clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        if (id == R.id.action_leaderboard) {
+            Toast.makeText(MainActivity.this, "Action : Leaderboard clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
