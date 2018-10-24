b87bd4a797df3c137ac129c2c0fca1548f3a2db5
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2018-10-24 16:34:25 +0530

    changing package name

---
+++ mobile/src/main/java/com/isumalab/learn/Leaderboard.java
@@ -0,0 +1,36 @@
+package com.isumalab.learn;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Leaderboard extends AppCompatActivity {
+
+    private Toolbar mTopToolbar1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+
+        //adding toolbar
+        mTopToolbar1 = (Toolbar) findViewById(R.id.my_back_toolbar);
+        setSupportActionBar(mTopToolbar1);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== android.R.id.home) {
+
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
