91195602932eb8b5a997d8ebd0533af3c4d1e734
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2018-12-27 16:03:13 +0530

    Update user information

--- mobile/src/main/java/com/isumalab/learn/EditUser.java
+++ mobile/src/main/java/com/isumalab/learn/EditUser.java
@@ -1,24 +1,35 @@
 package com.isumalab.learn;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 /*
-* This is for editing user details.
-*/
+ * This is for editing user details.
+ */
 public class EditUser extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar toolbarEditUser;
     FirebaseAuth mAuth;
+    DatabaseReference ref;
+    String fname, lname;
+    EditText firstName, lastName;
+    TextView update;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -27,7 +38,11 @@ public class EditUser extends AppCompatActivity implements View.OnClickListener 
 
         //adding toolbar
         toolbarEditUser = (Toolbar) findViewById(R.id.my_back_toolbar);
+        findViewById(R.id.update_user).setOnClickListener(this);
         findViewById(R.id.sign_out).setOnClickListener(this);
+
+        firstName = (EditText) findViewById(R.id.first_name);
+        lastName = (EditText) findViewById(R.id.last_name);
 
         setSupportActionBar(toolbarEditUser);
 
@@ -35,6 +50,22 @@ public class EditUser extends AppCompatActivity implements View.OnClickListener 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         mAuth = FirebaseAuth.getInstance();
+        ref = FirebaseDatabase.getInstance().getReference("/User/" + mAuth.getCurrentUser().getUid());
+
+        //setting text fields
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                firstName.setText(dataSnapshot.child("/firstName").getValue(String.class));
+                lastName.setText(dataSnapshot.child("/lastName").getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
     }
 
     @Override
@@ -53,8 +84,16 @@ public class EditUser extends AppCompatActivity implements View.OnClickListener 
                 FirebaseUser user = mAuth.getCurrentUser();
                 if (user != null) {
                     mAuth.signOut();
-                    Toast.makeText(this, user.getEmail()+ " Sign out!", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(this,MainActivity.class));
+                    Toast.makeText(this, user.getEmail() + " Sign out!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(this, MainActivity.class));
+                }
+                break;
+            case R.id.update_user:
+                if (!TextUtils.isEmpty(firstName.getText()) && !TextUtils.isEmpty(lastName.getText())) {
+                    fname = firstName.getText().toString();
+                    lname = lastName.getText().toString();
+                    ref.child("/firstName").setValue(fname);
+                    ref.child("/lastName").setValue(lname);
                 }
                 break;
         }
